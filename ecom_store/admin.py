c8d0c05e842605b3a894e14b8fe80c82082ac959
Leo <leo96pereira@hotmail.com>
2024-11-15 01:51:10 +0000

    Model and admin done

--- ecom_store/admin.py
+++ ecom_store/admin.py
@@ -1,3 +1,9 @@
-from django.contrib import admin
+from django.contrib import admin 
+from ecom_store.models import Orders
 
 # Register your models here.
+@admin.register(Orders)
+class OrdersAdmin(admin.ModelAdmin):
+    list_display = ['customer_name', 'product_name', 'quantity', 'order_date']
+    list_filter = ['customer_name', 'product_name', 'order_date']
+    show_facets = admin.ShowFacets.ALWAYS
--- ecom_store/models.py
+++ ecom_store/models.py
@@ -3,11 +3,15 @@ from django.db import models
 
 # Create your models here.
 
-class Store(models.Model): 
+class Orders(models.Model): 
     customer_name = models.CharField(max_length=100)
     product_name = models.CharField(max_length=100)
     quantity = models.IntegerField()
     order_date = models.DateField(default=timezone.now)
+
+    class Meta:
+        verbose_name_plural = 'Orders'
+        ordering = ['-order_date']
 
     def __str__(self): 
         return f"{self.customer_name} has ordered {self.product_name} // Quantity: {self.quantity} // On the {self.order_date}"
